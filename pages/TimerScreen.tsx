e9665b1bb81cbf383241a9d43a3ea5f3fcb4db64
Nsovo Ngobeni <97952201+MrSlovo@users.noreply.github.com>
2022-05-04 21:47:35 +0200

    created timer mvp
    Created a basic timer that can be triggered using any key presses

--- pages/TimerScreen.tsx
+++ pages/TimerScreen.tsx
@@ -1,11 +1,51 @@
 import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 import NavBar from './components/navbar';
+import Timer from './components/timer';
+import styles from '../styles/TimerScreen.module.css';
 
 const TimerScreen: NextPage = () => {
+	// Timer states
+	const [startTimeState, setStartTimeState] = useState(new Date().getTime());
+	const [timeElapsedState, setTimeElapsedState] = useState(0);
+	const [countState, setCountState] = useState(false);
+
+	// Increments counter
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (countState) setTimeElapsedState(timeElapsedState + 10);
+		}, 10);
+
+		return () => {
+			clearInterval(interval);
+		};
+	}, [timeElapsedState, countState]);
+
+	// Keyboard shorcuts
+	useEffect(() => {
+		function onKeyPress(event: KeyboardEvent) {
+			if (event.key) toggleCountState();
+			console.log('triggered');
+		}
+
+		window.addEventListener('keypress', onKeyPress);
+
+		return () => {
+			window.removeEventListener('keypress', onKeyPress);
+		};
+	}, [countState]);
+
+	/**
+	 * Toggles the count state changing whether or no the clock increments itself
+	 */
+	function toggleCountState() {
+		countState ? setCountState(false) : setCountState(true);
+	}
+
 	return (
-		<div>
+		<div onClick={toggleCountState} className={styles.container}>
 			<NavBar />
-			<h1>Timer Screen</h1>
+			<Timer timeElapsed={timeElapsedState} />
 		</div>
 	);
 };
---
+++ pages/components/timer.tsx
@@ -0,0 +1,44 @@
+import { CSSProperties } from 'react';
+import styles from '../../styles/timer.module.css';
+
+interface timerProps {
+	timeElapsed: number;
+}
+
+const Timer = (props: timerProps) => {
+	/**
+	 * Formats time elapsed into mm:ss format
+	 * @param milliSeconds The time elapsed in millisecods
+	 * @returns A string represnting the time passed in mm:ss format
+	 */
+	function formatTime(milliSeconds: number) {
+		const minutes = Math.floor((milliSeconds % (1000 * 60 * 60)) / (1000 * 60));
+		const seconds = Math.floor((milliSeconds % (1000 * 60)) / 1000);
+		const hundredths = Math.floor((milliSeconds % 1000) / 10);
+		return `${minutes ? pad(minutes) + ' : ' : ''}${pad(seconds)} : ${pad(
+			hundredths
+		)}`;
+	}
+
+	/**
+	 * Pads single digit number with 0
+	 * @param num A number
+	 * @returns A string of the number padded with a 0 if it's a single digit
+	 */
+	function pad(num: number) {
+		if (num < 10) {
+			return '0' + num;
+		}
+		return num + '';
+	}
+
+	return (
+		<div className={styles.timer}>
+			<div>
+				<h1>{formatTime(props.timeElapsed)}</h1>
+			</div>
+		</div>
+	);
+};
+
+export default Timer;
