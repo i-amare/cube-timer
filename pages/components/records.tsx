134135a718d26b2e3fa71f966d70d35bd32503b5
Nsovo Ngobeni <97952201+MrSlovo@users.noreply.github.com>
2022-05-09 06:08:01 +0200

    Update records.tsx

--- pages/components/records.tsx
+++ pages/components/records.tsx
@@ -9,15 +9,15 @@ interface recordsProps {
 const Records = (props: recordsProps) => {
 	return (
 		<div className={styles.record} style={props.index % 2 ? { backgroundColor: '#ffffff05' } : {}}>
-			<div className={styles.cell} >
+			<div>
 				<h5>SOLVE</h5>
 				<p style={{ color: 'cornflowerblue' }}>{props.index}</p>
 			</div>
-			<div className={styles.cell} >
+			<div>
 				<h5>SCRAMBLE</h5>
 				<p style={{ color: 'grey' }}>{props.algorithm.join(" ")}</p>
 			</div>
-			<div className={styles.cell} >
+			<div>
 				<h5>TIME</h5>
 				<p style={{ color: 'lightgreen' }}>{props.time}</p>
 			</div>
