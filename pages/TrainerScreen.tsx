0b8c91baccb5d23d0e14a094c7148537a3d3d92c
Nsovo Ngobeni <97952201+MrSlovo@users.noreply.github.com>
2022-05-09 06:08:25 +0200

    Created trainer screen demo

--- pages/TrainerScreen.tsx
+++ pages/TrainerScreen.tsx
@@ -1,11 +1,43 @@
 import { NextPage } from "next";
 import NavBar from "./components/navbar";
+import AlgorithmCell from "./components/algorithm-cell";
+import styles from "../styles/TrainerScreen.module.css";
 
 const TrainerScreen: NextPage = () => {
+	const cells = []
+	for (let i = 0; i < 35; i++) {
+		cells.push('')
+	}
+
 	return (
-		<div className="SolveScreen">
+		<div className={styles.timerScreen} >
 			<NavBar />
-			<h1>TRAINER</h1>
+			<div className={styles.algs}>
+				{
+					cells.map((val: any, idx: number) => {
+						return (
+							<AlgorithmCell name={`${idx + 1}`} algorithm={[
+								"R'",
+								"U",
+								"B",
+								"F'",
+								"L",
+								"U",
+								"B",
+								"D",
+								"L'",
+								"B'",
+								"D",
+								"B",
+								"B",
+								"L'",
+								"U'"
+							]} avergeTime={6.72} bestTime={4.76} group='Dot' lighten={idx} />
+						)
+					})
+				}
+
+			</div>
 		</div>
 	)
 }
--- pages/components/algorithm-cell.tsx
+++ pages/components/algorithm-cell.tsx
@@ -1,13 +1,39 @@
+import styles from "../../styles/algorithm-cell.module.css";
+
 interface algorithmCellProps {
 	name: string,
 	group: string,
 	algorithm: string[],
 	bestTime: number,
 	avergeTime: number,
+	lighten: number;
 }
 
-const AlgorithmCell = () => {
-
+const AlgorithmCell = (props: algorithmCellProps) => {
+	return (
+		<div className={styles.cell} style={props.lighten % 2 ? { backgroundColor: '#ffffff05' } : {}} >
+			<div>
+				<h5>NAME</h5>
+				<p>{props.name}</p>
+			</div>
+			<div>
+				<h5>GROUP</h5>
+				<p>{props.group}</p>
+			</div>
+			<div>
+				<h5>ALGORITHM</h5>
+				<p>{props.algorithm.join(" ")}</p>
+			</div>
+			<div>
+				<h5>BEST</h5>
+				<p>{props.bestTime}</p>
+			</div>
+			<div>
+				<h5>AVG</h5>
+				<p>{props.avergeTime}</p>
+			</div>
+		</div>
+	)
 }
 
 export default AlgorithmCell;
